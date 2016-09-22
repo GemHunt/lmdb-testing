343f63c582cc603eed4d6884683bb4c256b6e4c6
Paul Krush <pkrush@GemHunt.com>
2016-09-22 10:06:46 -0500

    added script from digits

--- create-lmdb.py
+++ create-lmdb.py
@@ -11,36 +11,30 @@ import cv2
 # sys.path.append('~/caffe/python') using the ~ does not work, for some reason???
 sys.path.append('/home/pkrush/caffe/python')
 import caffe
-N = 2034 * 100
 
-# We need to prepare the database for the size. We'll set it 10 times
-# greater than what we theoretically need. There is little drawback to
-# setting this too big. If you still run into problem after raising
-# this, you might want to try saving fewer entries in a single
-# transaction.
-map_size = 1000000000
+map_size = 3000000000
 env = lmdb.open('mylmdb', map_size=map_size)
 id = 0
 
 with env.begin(write=True) as txn:
     # txn is a Transaction object
     for filename in glob.iglob('/home/pkrush/2-camera-scripts/crops/*.png'):
-        imageID = filename[-9:]
-        imageID = imageID[:5]
+        print filename
         crop = cv2.imread(filename)
         if crop is None:
             continue
-        crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+        #crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
         crop = cv2.resize(crop, (280, 280), interpolation=cv2.INTER_AREA)
 
         for x in range(0,9):
             for y in range(0,9):
                 crop_crop = crop[y * 28 :(y + 1) * 28,x * 28 :(x + 1) * 28]
-                datum = caffe.proto.caffe_pb2.Datum()
-                datum.channels = 1
-                datum.height = 28
-                datum.width = 28
-                datum.data = crop_crop.tostring()
+                datum = caffe.io.array_to_datum(crop_crop, -1)
+                #datum = caffe.proto.caffe_pb2.Datum()
+                #datum.channels = 1
+                #datum.height = 28
+                #datum.width = 28
+                #datum.data = crop_crop.tostring()
                 datum.label = x * 10 + y
                 id += 1
                 str_id = '{:08}'.format(id)
