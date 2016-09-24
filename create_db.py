9b34174a53927676d2dbe2c2253c95b486b8d95f
Paul Krush <pkrush@GemHunt.com>
2016-09-23 22:01:34 -0500

    The lmdbs work when you replace them with ones in the digits jobs files. What a hack.

--- create_db.py
+++ create_db.py
@@ -50,7 +50,7 @@ def create_lmdbs():
         test.png
     """
 
-    max_images = 100
+    max_images = 10000
     crop_size = 280
     folder = 'lmdb-test'
 
@@ -59,39 +59,44 @@ def create_lmdbs():
         image_db = lmdb.open(os.path.join(folder, '%s_images' % phase),
                 map_async=True,
                 max_dbs=0)
-        label_db = lmdb.open(os.path.join(folder, '%s_labels' % phase),
-                map_async=True,
-                max_dbs=0)
+        #label_db = lmdb.open(os.path.join(folder, '%s_labels' % phase),
+                #map_async=True,
+                #max_dbs=0)
 
         # add up all images to later create mean image
         image_sum = np.zeros((1,28,28), 'float64')
 
         # arrays for image and label batch writing
         image_batch = []
-        label_batch = []
+        #label_batch = []
         id = -1
 
         for filename in glob.iglob('/home/pkrush/2-camera-scripts/crops/*.png'):
+            imageid = filename[-9:]
+            imageid = imageid[:5]
             id += 1
             if id > max_images - 1:
                 continue
             if ((id+4) % 4 == 0) and phase == 'train' :
-                print 'not train'
                 continue
             if ((id+4) % 4 != 0) and phase == 'val':
-                print 'not val'
                 continue
 
-            print id
             crop = cv2.imread(filename)
             if crop is None:
                 continue
             crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
             crop = cv2.resize(crop, (crop_size, crop_size), interpolation=cv2.INTER_AREA)
 
+            #os.makedirs('lmdb-files')
+            #for x in range(0, 10):
+                #for y in range(0, 10):
+                    #os.makedirs('lmdb-files/' + str(x * 10 + y))
+
             for x in range(0, 10):
                 for y in range(0, 10):
                     crop_crop = crop[y * 28:(y + 1) * 28, x * 28:(x + 1) * 28]
+                    #cv2.imwrite('lmdb-files/' + str(x * 10 + y) + '/' + imageid + '.png', crop_crop)
                     crop_crop = np.reshape(crop_crop, (1,28,28))
                     image_sum += crop_crop
                     label = x * 10 + y
@@ -104,26 +109,27 @@ def create_lmdbs():
                     image_batch.append([str_id, datum])
 
                     # create label Datum
-                    label_datum = caffe_pb2.Datum()
-                    label_datum.channels, label_datum.height, label_datum.width = 1, 1, 1
-                    label_datum.float_data.extend(np.array([label]).flat)
-                    label_batch.append([str_id, label_datum])
+                    #label_datum = caffe_pb2.Datum()
+                    #label_datum.channels, label_datum.height, label_datum.width = 1, 1, 1
+                    #label_datum.float_data.extend(np.array([label]).flat)
+                    #label_batch.append([str_id, label_datum])
 
 
         # close databases
         _write_batch_to_lmdb(image_db, image_batch)
-        _write_batch_to_lmdb(label_db, label_batch)
+        #_write_batch_to_lmdb(label_db, label_batch)
         image_batch = []
-        label_batch = []
+        #label_batch = []
 
         image_db.close()
-        label_db.close()
+        #label_db.close()
 
         # save mean
         mean_image = (image_sum / id*100).astype('uint8')
         _save_mean(mean_image, os.path.join(folder, '%s_mean.binaryproto' % phase))
         #mean_image = np.reshape(mean_image, (28, 28))
         #_save_mean(mean_image, os.path.join(folder, '%s_mean.png' % phase))
+        print id
 
     return
 
