00e9d467e6044fcf422c9e510dadbb92d2e0b53b
Paul Krush <pkrush@GemHunt.com>
2016-11-08 17:04:33 -0600

    Renamed crops

--- self_supervised.py
+++ self_supervised.py
@@ -1,5 +1,33 @@
-import cPickle
+import cPickle as pickle
 import random
-index = [random.randint(1000, 14321) for x in range(25)]
-print index
+import glob
+import os
+
+data_dir = '/home/pkrush/lmdb-files/data/'
+crop_dir = '/home/pkrush/lmdb-files/crops/'
+index_filename = data_dir + 'index.p'
+
+
+def create_index():
+    index = [random.randint(1000, 14321) for x in range(25)]
+    pickle.dump(index, open(index_filename , "wb") )
+
+def get_index():
+    index = pickle.load( open(index_filename , "rb" ) )
+
+def rename_crops():
+    crops = []
+    for filename in glob.iglob(crop_dir + '*.jpg'):
+        crops.append([random.random(),filename])
+    crops.sort()
+    pickle.dump(crops, open(data_dir + 'copper_crops.p', "wb"))
+    key = 0
+    for rand,filename in crops:
+        key += 1
+        os.rename(filename, crop_dir + str(key) + '.jpg')
+
+
+rename_crops()
+
+
 
